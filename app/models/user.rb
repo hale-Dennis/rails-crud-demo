6e1163e90f8ebc56b02e5d73e88de64af020ddb1
Dennis Owusu <haledennis0@gmail.com>
2025-05-26 15:05:35 +0000

    feat: add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-
+  validates :username, presence: true, length: { minimum: 5, maximum: 20 }, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 }, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
 end
